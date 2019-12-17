ca8b5fa148118413704df1d8293394f9c50e07d8
Ajay Singh <ajayh2@gmail.com>
2019-12-17 01:19:00 -0500

    removed comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,50 @@
 $(document).ready(function () {
 
-// local buttons
     var localButtons = ["Arnold Schwarzenegger","Michael J Fox","Lucille Ball","Taylor Swift","Samuel Jackson",]
 
-    // Event listener for all button elements
     var button;
     var gifSavedButtons = "";
 
     $("button").on("click", function() {
-    // In this case, the "this" keyword refers to the button that was clicked
     var person = $(this).attr("data-person");
 
-    // Constructing a URL to search Giphy for the name of the person who said the quote
     var queryURL =
         "https://api.giphy.com/v1/gifs/search?q=" +
         person +
         "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-    // Performing our AJAX GET request
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        // After the data comes back from the API
         .then(function(response) {
-        // Storing an array of results in the results variable
         var results = response.data;
 
-        // Looping over every result item
         for (var i = 0; i < results.length; i++) {
-            // Only taking action if the photo has an appropriate rating
-            // Creating a div for the gif
             var gifDiv = $("<div>");
 
-            // Storing the result item's rating
             var rating = results[i].rating;
 
-            // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + rating);
 
-            // Creating an image tag
             var personImage = $("<img>");
 
-            // gives still attribute
             personImage.attr("src", results[i].images.fixed_height_still.url);
             personImage.attr("data-still", results[i].images.fixed_height_still.url);
             personImage.attr("data-animate", results[i].images.fixed_height.url);
             personImage.attr("data-state", "still");
             personImage.addClass("gif");
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
 
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
             gifDiv.append(p);
             gifDiv.append(personImage);
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
             $("#gifs-appear-here").prepend(gifDiv);
         }
         });
     });
 
     $("#gifs-appear-here").on("click", ".gif", function() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
