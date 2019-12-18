7b534f4c05685a4ef1b80f1c55af0d575113d8e1
Ajay Singh <ajayh2@gmail.com>
2019-12-17 20:48:43 -0500

    adding movie giphy code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,71 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  var localButtons = [
+    "Batman Begins",
+    "The Dark Knight",
+    "Dark Knight Rises",
+    "Breakfast at Tiffany's",
+    "Once Upon a Time in Hollywood",
+    "Joker",
+    "Lighthouse"
+  ];
 
-    var localButtons = ["Arnold Schwarzenegger","Michael J Fox","Lucille Ball","Taylor Swift","Samuel Jackson",]
+  function displayInfo() {
+    var movie = $(this).attr("movie-name");
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      movie +
+      "&api_key=dc6zaTOxFJmzC&limit=10";
+  }
 
-    var button;
-    var gifSavedButtons = "";
-
-    $("button").on("click", function() {
+  $("button").on("click", function() {
     var person = $(this).attr("data-person");
 
     var queryURL =
-        "https://api.giphy.com/v1/gifs/search?q=" +
-        person +
-        "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      person +
+      "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function(response) {
-        var results = response.data;
-        console.log(response.data)
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.data;
+      console.log(response.data);
 
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
 
-            var rating = results[i].rating;
+        var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+        var p = $("<p>").text("Rating: " + rating);
 
-            var personImage = $("<img>");
+        var personImage = $("<img>");
 
-            personImage.attr("src", results[i].images.fixed_height_still.url);
-            personImage.attr("data-still", results[i].images.fixed_height_still.url);
-            personImage.attr("data-animate", results[i].images.fixed_height.url);
-            personImage.attr("data-state", "still");
-            personImage.addClass("gif");
+        personImage.attr("src", results[i].images.fixed_height_still.url);
+        personImage.attr(
+          "data-still",
+          results[i].images.fixed_height_still.url
+        );
+        personImage.attr("data-animate", results[i].images.fixed_height.url);
+        personImage.attr("data-state", "still");
+        personImage.addClass("gif");
 
-            gifDiv.append(p);
-            gifDiv.append(personImage);
+        gifDiv.append(p);
+        gifDiv.append(personImage);
 
-            $("#gifs-appear-here").prepend(gifDiv);
-        }
-        });
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
     });
+  });
 
-    $("#gifs-appear-here").on("click", ".gif", function() {
+  $("#gifs-appear-here").on("click", ".gif", function() {
     var state = $(this).attr("data-state");
     if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
     } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
     }
-    });
+  });
 });
