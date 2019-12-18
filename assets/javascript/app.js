00a6e2078132c47cc69335d960c1f966ee525237
Ajay Singh <ajayh2@gmail.com>
2019-12-18 00:10:21 -0500

    buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,87 @@
 $(document).ready(function() {
-  var localButtons = [
-    "Batman Begins",
-    "The Dark Knight",
-    "Dark Knight Rises",
-    "Breakfast at Tiffany's",
-    "Once Upon a Time in Hollywood",
-    "Joker",
-    "Lighthouse"
-  ];
-
-  function displayInfo() {
-    var movie = $(this).attr("movie-name");
-    var queryURL =
-      "http://api.giphy.com/v1/gifs/search?q=" +
-      movie +
-      "&api_key=dc6zaTOxFJmzC&limit=10";
-  }
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+    var localChanges = [
+      "Batman Begins",
+      "The Dark Knight",
+      "Dark Knight Rises",
+      "Breakfast at Tiffany's",
+      "Once Upon a Time in Hollywood",
+      "Joker",
+      "Lighthouse"
+    ];
+  
+    function displayInfo() {
+      var movie = $(this).attr("movie-name");
+      var queryURL =
+        "http://api.giphy.com/v1/gifs/search?q=" +
+        movie +
+        "&api_key=dc6zaTOxFJmzC&limit=10";
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
         $("#movies").empty();
-
-      var results = response.data;
-      console.log(response.data);
-
-      var results = response.data;
-      
-      for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div>");
-
-        var rating = results[i].rating;
-
-        var p = $("<p>").text("Rating: " + rating);
-
-        var gif = $("<img>");
-
-        gif.attr("src", results[i].images.fixed_height_still.url);
-        gif.attr(
-          "data-still",
-          results[i].images.fixed_height_still.url
-        );
-        gif.attr("data-animate", results[i].images.fixed_height.url);
-        gif.attr("data-state", "still");
-        gif.addClass("gif");
-
-        gifDiv.append(p);
-        gifDiv.append(gif);
-
-        $("movies").prepend(gifDiv);
-      }
-    });
-
-
-  $(".gif").on("click", function() {
-    var state = $(this).attr("data-state");
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+  
+        var results = response.data;
+  
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div class='personMovie'>");
+  
+          var rating = results[i].rating;
+          var pRate = $("<p>").text("Rating: " + rating);
+  
+          var urlStill = results[i].images.fixed_height_still.url;
+          var urlPlay = results[i].images.fixed_height.url;
+  
+          var gif = $("<img>")
+            .addClass("gif")
+            .attr("src", urlStill)
+            .attr("data-still", urlStill)
+            .attr("data-animate", urlPlay)
+            .attr("data-state", "still");
+  
+          gifDiv.append(gif);
+          gifDiv.append(pRate);
+  
+          $("#movies").append(gifDiv);
+        }
+        $(".gif").on("click", function() {
+          var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+      });
     }
-  });
-
+  
     function renderButtons() {
-        $("#movieButtons").empty();
-        for (var i = 0; i < localChanges.length; i++) {
+      $("#movieButtons").empty();
+      for (var i = 0; i < localChanges.length; i++) {
         var movieRender = $("<button>");
         movieRender.addClass("movie");
         movieRender.attr("movie-name", localChanges[i]);
         movieRender.text(localChanges[i]);
         $("#movieButtons").append(movieRender);
-        }
+      }
     }
-
+  
     $("#addMovie").on("click", function(event) {
-        event.preventDefault();
-        var movie = $("#movie-input")
+      event.preventDefault();
+      var movie = $("#movie-input")
         .val()
         .trim();
-
-        localChanges.push(movie);
-        $("#movie-input").val(" ");
-        renderButtons();
+  
+      localChanges.push(movie);
+      $("#movie-input").val(" ");
+      renderButtons();
     });
-
+  
     $(document).on("click", ".movie", displayInfo);
-
+  
     renderButtons();
-    });
-
-});
+  });
+  
